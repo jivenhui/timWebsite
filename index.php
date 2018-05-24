5ea798259da82b8e5e8d58eb8e56a7e60932950e
LAPTOP-U3NBCKVF\Jari Venhuis <jari.venhuis@gmail.com>
2018-05-24 14:35:20 +0200

    ww

---
+++ index.php
@@ -0,0 +1,101 @@
+<html>
+<head>
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <!-- jQuery library -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <!-- Latest compiled JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+  <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body class="body">
+  <header>
+    <div class="text-center">
+      <img class="logo" src="images\cloudLogo.png" alt="">
+    </div>
+    <nav >
+      <ul class="container">
+        <li class=""><a href="home.html">Thuis</a></li>
+        <li><a href="about.html">Spelletjes</a></li>
+        <li><a href="services.html">Recepten</a></li>
+        <li><a href="products.html">Chat</a></li>
+        <li><a href="jobs.html">Weetjes</a></li>
+        <li><a href="contact.html">Contact</a></li>
+      </ul>
+    </nav>
+    <header>
+      <div class="content">
+        <div class="row">
+          <div class="col-md-4">
+            <h1 class=" text-center">Recepten met wolkjes: </h1>
+            <img class="bessenMeringue" src="images\bessenMeringue.jpg" alt="">
+          </div>
+          <div class="col-md-5 text-center">
+            <h1 class="text-center">Schrijf je nu in!</h1>
+            <img class="kidsthumbsup" src="images\kidsthumbsup.jpg" alt="">
+          </div>
+          <div class="col-md-3">
+            <h2 class="text-center ">Laat een reactie achter!</h2>
+            <div class="chatWrapper">
+
+
+              <div class="chat ">
+                <p class="text-left">Hoi, ik ben nieuw hier, wat lekker die Wolkjes.</p>
+                <span class="time-left">11:01</span>
+              </div>
+
+              <div class="chat darker">
+                <p class="text-right">Ja lekker he, heb je de toetjes van deze site al geprobeerd?</p>
+                <span class="time-right">11:02</span>
+              </div>
+
+              <div class="chat ">
+                <p class="text-left">Nee, ziet er wel heel lekker uit, ik ga meteen aan mama vragen of ik iets mag maken!</p>
+                <span class="time-left">11:05</span>
+              </div>
+              <div class="chat darker">
+                <p class="text-right">Ik heb de 2e gisteren gemaakt die was erg lekker.</p>
+                <span class="time-right">11:7</span>
+              </div>
+              <div class="chat ">
+                <p class="text-left">Ow, dan ga ik die ook proberen.😊</p>
+                <span class="time-left">11:10</span>
+              </div>
+              <div class="chat darker">
+                <p class="text-right">Succes! Stuur hier maar een foto op als het gelukt is!</p>
+                <span class="time-right">11:11</span>
+              </div>
+              <div class="chat ">
+                <p class="text-left">Dankjewel, zal ik doen hoor!</p>
+                <span class="time-left">11:12</span>
+              </div>
+              <div>
+                <form action="search.php" method="get">
+                    <input type="submit" name="search" value="Go" style="float: right; height: 55px;" />
+                  <div style="overflow: hidden; padding-right: .5em;">
+                    <textarea rows="2.5" cols="45">Enter text here...</textarea>
+                  </div>
+                </form>
+
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-7">
+            <h1 class=" text-center">Ontbijtspel </h1>
+            <img class="bessenMeringue" src="images\ontbijt.jpg" alt="">
+          </div>
+          <div class="col-md-5 text-center">
+            <h1 class="text-center">Schrijf je nu in!</h1>
+            <img class="kidsthumbsup" src="images\wolkjesNieuws.png" alt="">
+          </div>
+        </div>
+      </div>
+      <footer class="text-center">
+    <h4>©2018 | Wolkjes.nl | privacy | statement | contact | developed by Venhuis</h4>
+      </footer>
+    </body>
+    </html>
